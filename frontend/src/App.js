7d26093019ff686f6e80fcfad83e13027cffd129
Chi Chen <cc2015ex@gmail.com>
2020-08-02 22:11:10 -0400

    Add component Modal

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
+import Modal from "./components/Modal";
 
-const taskItems = [
+const todoItems = [
   {
     id: 1,
     title: "Go to Market",
@@ -26,15 +27,37 @@ const taskItems = [
     completed: false
   }
 ];
-
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      modal: false,
       viewCompleted: false,
-      taskList: taskItems
+      activeItem: {
+        title: "",
+        description: "",
+        completed: false
+      },
+      todoList: todoItems
     };
   }
+  toggle = () => {
+    this.setState({ modal: !this.state.modal });
+  };
+  handleSubmit = item => {
+    this.toggle();
+    alert("save" + JSON.stringify(item));
+  };
+  handleDelete = item => {
+    alert("delete" + JSON.stringify(item));
+  };
+  createItem = () => {
+    const item = { title: "", description: "", completed: false };
+    this.setState({ activeItem: item, modal: !this.state.modal });
+  };
+  editItem = item => {
+    this.setState({ activeItem: item, modal: !this.state.modal });
+  };
   displayCompleted = status => {
     if (status) {
       return this.setState({ viewCompleted: true });
@@ -61,8 +84,8 @@ class App extends Component {
   };
   renderItems = () => {
     const { viewCompleted } = this.state;
-    const newItems = this.state.taskList.filter(
-      item => item.completed == viewCompleted
+    const newItems = this.state.todoList.filter(
+      item => item.completed === viewCompleted
     );
     return newItems.map(item => (
       <li
@@ -70,16 +93,26 @@ class App extends Component {
         className="list-group-item d-flex justify-content-between align-items-center"
       >
         <span
-          className={`task-title mr-2 ${
-            this.state.viewCompleted ? "completed-task" : ""
+          className={`todo-title mr-2 ${
+            this.state.viewCompleted ? "completed-todo" : ""
           }`}
           title={item.description}
         >
           {item.title}
         </span>
         <span>
-          <button className="btn btn-secondary mr-2"> Edit </button>
-          <button className="btn btn-danger">Delete </button>
+          <button
+            onClick={() => this.editItem(item)}
+            className="btn btn-secondary mr-2"
+          >
+            Edit
+          </button>
+          <button
+            onClick={() => this.handleDelete(item)}
+            className="btn btn-danger"
+          >
+            Delete
+          </button>
         </span>
       </li>
     ));
@@ -87,12 +120,14 @@ class App extends Component {
   render() {
     return (
       <main className="content">
-        <h1 className="text-white text-uppercase text-center my-4">Task app</h1>
+        <h1 className="text-white text-uppercase text-center my-4">Todo app</h1>
         <div className="row ">
           <div className="col-md-6 col-sm-10 mx-auto p-0">
             <div className="card p-3">
               <div className="">
-                <button className="btn btn-primary">Add task</button>
+                <button onClick={this.createItem} className="btn btn-primary">
+                  Add task
+                </button>
               </div>
               {this.renderTabList()}
               <ul className="list-group list-group-flush">
@@ -101,6 +136,13 @@ class App extends Component {
             </div>
           </div>
         </div>
+        {this.state.modal ? (
+          <Modal
+            activeItem={this.state.activeItem}
+            toggle={this.toggle}
+            onSave={this.handleSubmit}
+          />
+        ) : null}
       </main>
     );
   }
